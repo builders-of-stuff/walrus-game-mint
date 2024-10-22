1a50f92a6ba69b5137a3bfc205836c9a5c7c4556
Kyle Truong <ktruong008@gmail.com>
2024-10-22 17:25:22 -0400

    update for testnet

--- src/lib/shared/shared.stuff.ts
+++ src/lib/shared/shared.stuff.ts
@@ -6,7 +6,7 @@ const BASE36 = '0123456789abcdefghijklmnopqrstuvwxyz';
 // const MINT_SITE_OBJECT_ID =
 //   '0x58708dac4a7f4ae873c051e7191bb8674cafb4486a69913242a9582405f4ea20';
 export const WALRUS_GAME_PACKAGE_ID =
-  '0x9386430b67ebe8409dfba5a5be7e1963466439de7195ce736046631992e043c2';
+  '0x72f4de4be8e8943d855a98ad5f6954f370671bf03865c019a72ae1ccccb3cc52';
 
 const b36 = baseX(BASE36);
 
