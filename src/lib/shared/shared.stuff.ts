44f2acbbbcebdb95f4ee7bcc7f12a3850e55313d
Kyle Truong <ktruong008@gmail.com>
2025-01-25 16:43:02 -0500

    update refs

--- src/lib/shared/shared.stuff.ts
+++ src/lib/shared/shared.stuff.ts
@@ -6,10 +6,13 @@ const BASE36 = '0123456789abcdefghijklmnopqrstuvwxyz';
 // const MINT_SITE_OBJECT_ID =
 //   '0x58708dac4a7f4ae873c051e7191bb8674cafb4486a69913242a9582405f4ea20';
 export const WALRUS_GAME_PACKAGE_ID =
-  '0x72f4de4be8e8943d855a98ad5f6954f370671bf03865c019a72ae1ccccb3cc52';
+  '0xc314dece944a590e0de2192befbb289b4522d0f5417acc05029c86b48e7d9818';
 
 const b36 = baseX(BASE36);
 
 export function walrusLink(id: string): string {
   return 'https://' + b36.encode(fromHEX(id.substring(2))) + '.walrus.site';
 }
+
+// 0xb5d80c3b9b7a9a66f57e21e7741e3ee385a65faeca5a02701e359f7a5578808a
+// https://4j5tbcmxs58v45u0w927p2vzmed5zfhnbrhe29oz699h8872iy.walrus.site
